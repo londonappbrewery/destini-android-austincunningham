ea3569f79379335cc55e29bb2f9f4b295d5424e5
Austin Cunningham <aucunnin@redhat.com>
2017-12-01 21:57:00 +0000

    challange completed

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,12 +1,22 @@
 package com.londonappbrewery.destini;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mStory;
+    Button mTopButton;
+    Button mBottomButton;
 
+    int mStoryTracker = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +25,80 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStory = (TextView)findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button)findViewById(R.id.buttonBottom);
+
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Log.d("Destini", "Top button clicked ");
+                boolean mTop = true;
+                updateStory(mTop);
+            }
+
+        });
+
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Destini", "Bottom button clicked ");
+                boolean mBottom = false;
+                updateStory(mBottom);
+            }
+        });
+
+    }
+
+    private void updateStory(boolean topOrBottom){
 
 
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        if (topOrBottom && mStoryTracker == 2 ){
+            mStory.setText(R.string.T3_Story);
+            mTopButton.setText(R.string.T3_Ans1);
+            mBottomButton.setText(R.string.T3_Ans2);
+            mStoryTracker = 3;
+        } else if (topOrBottom && mStoryTracker == 3){
+            mStory.setText(R.string.T6_End);
+            endAlert();
+            mTopButton.setText("Good bye");
+            mBottomButton.setText("Story over");
+        } else if (!topOrBottom && mStoryTracker == 3){
+            mStory.setText(R.string.T5_End);
+            endAlert();
+            mTopButton.setText("Good bye");
+            mBottomButton.setText("Story over");
+        } else if (!topOrBottom && mStoryTracker == 2){
+            mStory.setText(R.string.T4_End);
+            endAlert();
+            mTopButton.setText("Good bye");
+            mBottomButton.setText("Story over");
+        } else if (topOrBottom ){
+            mStory.setText(R.string.T3_Story);
+            mTopButton.setText(R.string.T3_Ans1);
+            mBottomButton.setText(R.string.T3_Ans2);
+            mStoryTracker = 3;
+        } else if(!topOrBottom) {
+            mStory.setText(R.string.T2_Story);
+            mTopButton.setText(R.string.T2_Ans1);
+            mBottomButton.setText(R.string.T2_Ans2);
+            mStoryTracker = 2;
+        }
 
+    }
 
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+    private void endAlert(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Game over");
+        alert.setCancelable(false);
+        alert.setMessage("So long and thanks for all the fish  !!!!!");
+        alert.setPositiveButton("Restart Adventure", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                recreate();
+            }
+        });
+        alert.show();
     }
 }
